2e943d658f1d9076705872d33d3d8b3370cafcdf
Lukas Simonson <lukas@hydradesignllc.com>
2023-01-30 17:16:36 -0700

    Adds Optional Backbutton Parameter

--- Sources/NavigationRouter/NavigatesTo.swift
+++ Sources/NavigationRouter/NavigatesTo.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 public extension View {
-    func navigatesTo<Content: View>(_ type: Content.Type) -> some View {
+    func navigatesTo<Content: View>(_ type: Content.Type, usesBackButton: Bool = true) -> some View {
         navigationDestination(for: NavigationHandler.NavLocation<Content>.self) { location in
-            location.view.modifier(NavigatesTo())
+            location.view.modifier(NavigatesTo(backButton: usesBackButton))
         }
     }
 }
@@ -18,13 +18,24 @@ public extension View {
 private struct NavigatesTo: ViewModifier {
     
     @NavRouter var router
+    var backButton: Bool
     
     func body(content: Content) -> some View {
+        if backButton { usesToolbar(content: content) }
+        else { noToolbar(content: content) }
+    }
+    
+    func noToolbar(content: Content) -> some View {
+        content
+            .navigationBarBackButtonHidden()
+    }
+    
+    func usesToolbar(content: Content) -> some View {
         content
             .navigationBarBackButtonHidden()
             .toolbar {
                 if router.navPath.count > 0 {
-                    
+
                     #if os(macOS)
                     let placement = ToolbarItemPlacement.navigation
                     #else
@@ -42,4 +53,5 @@ private struct NavigatesTo: ViewModifier {
                 }
             }
     }
+    
 }
